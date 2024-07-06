6124eb9cdca7e81b6dea3963fd6b7d43bd9150f0
Hamada Abdelaal <hamada_abdelaal@hotmail.com>
2024-07-06 08:47:29 +0200

    all requirements

--- src/router.js
+++ src/router.js
@@ -1,25 +1,55 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
 const mockUser = {
-    username: 'authguy',
-    password: 'mypassword',
-    profile: {
-        firstName: 'Chris',
-        lastName: 'Wolstenholme',
-        age: 43
-    }
+  username: "authguy",
+  password: "mypassword",
+  profile: {
+    firstName: "Chris",
+    lastName: "Wolstenholme",
+    age: 43,
+  },
 };
 
-router.post('/login', (req, res) => {
+const payload = { username: mockUser.username };
 
+router.post("/login", (req, res) => {
+  //requirement 1
+  // const token = jwt.sign(payload, process.env.JWT_SECRET);
+  // res.status(201).json({ token });
+
+  // requirement 3
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ message: "username and password are required" });
+  }
+
+  if (username !== mockUser.username || password !== mockUser.password) {
+    return res
+      .status(401)
+      .json({ message: "username or password is incorrect" });
+  }
+
+  const token = jwt.sign(payload, process.env.JWT_SECRET);
+  res.status(200).json({ token });
 });
 
-router.get('/profile', (req, res) => {
-  
-});
+router.get("/profile", (req, res) => {
+  // here we use 'extractToken' middleware that we created in server.js
+  if (!req.token) {
+    return res.status(401).json({ message: "No token provided" });
+  }
 
+  jwt.verify(req.token, process.env.JWT_SECRET, (error) => {
+    if (error) {
+      return res.status(403).json({ message: "Failed to authenticate token" });
+    }
+    res.json({ profile: mockUser.profile });
+  });
+});
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,18 +1,32 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const cors = require('cors');
-const morgan = require('morgan');
+const cors = require("cors");
+const morgan = require("morgan");
 
-app.disable('x-powered-by');
+app.disable("x-powered-by");
 
-app.use(morgan('dev'))
+// middleware to extract token from authorization header
+function extractToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  if (authHeader && authHeader.startsWith("Bearer ")) {
+    const token = authHeader.split(" ")[1];
+    req.token = token;
+  } else {
+    req.token = null;
+  }
+  next();
+}
+
+app.use(extractToken);
+
+app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const router = require('./router');
+const router = require("./router");
 
-app.use('/', router);
+app.use("/", router);
 
-module.exports = app
+module.exports = app;
